3a427182cf74073dfb7f39b1f996e2dfe8584d68
Salma5806 <5806@holbertonstudents.com>
2023-03-25 23:57:56 +0100

    states

--- models/state.py
+++ models/state.py
@@ -11,3 +11,14 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+@property
+    def cities(self):
+        """getter attribute cities that returns the list of City"""
+        from models import storage
+        my_list = []
+        extracted_cities = storage.all(City).values()
+        for city in extracted_cities:
+            if self.id == city.state_id:
+                my_list.append(city)
+        return my_list

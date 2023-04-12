513a26adb1d75979545ba02504519ecfb7a46615
nadaMAoui <maaouinada747@gmail.com>
2023-04-13 01:57:22 +0200

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""
-initialiation flask application instance
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
 """
 from flask import Flask
 
@@ -8,9 +10,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def Hello_HBNB():
-    """view function'"""
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0') 
-
+   app.run(host='0.0.0.0') 

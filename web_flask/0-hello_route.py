85393e337c5d95076efb4667cf4f41c5eeae379c
nadaMAoui <maaouinada747@gmail.com>
2023-04-13 01:54:40 +0200

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-'''
+"""
 initialiation flask application instance
-'''
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def Hello_HBNB():
+    """view function'"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0') 

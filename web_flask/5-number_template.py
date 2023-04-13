dafe9e43ff6e692c16766a2c81d2f8f48b597721
nadaMAoui <maaouinada747@gmail.com>
2023-04-14 00:55:57 +0200

    task 5 corr

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -58,7 +58,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """Displays HTML page if n is int"""
-    return render_template("5-number.html", number=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":

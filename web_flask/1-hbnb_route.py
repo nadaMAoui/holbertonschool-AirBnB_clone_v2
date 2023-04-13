be7cffaf805d53e3a2793e670698806dd92b3579
nadaMAoui <maaouinada747@gmail.com>
2023-04-13 02:29:10 +0200

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,24 +1,25 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 """
-This module is a simple Flask application that
-displays two routes: a homepage ("/") and an HBNB page ("/hbnb")
+This module is a simple Flask application
+that displays two routes: a homepage ("/") and an HBNB page ("/hbnb").
 """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slaches=False)
+@app.route("/", strict_slashes=False)
 def Hello_HBNB():
     '''view function'''
     return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slaches=False)
+@app.route("/hbnb", strict_slashes=False)
 def HBNB():
     '''view function '''
     return 'HBNB'
-    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

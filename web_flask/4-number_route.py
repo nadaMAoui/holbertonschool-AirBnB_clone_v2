e74081fefcdc08481f13ff19ecaac8ff0fdffda4
nadaMAoui <maaouinada747@gmail.com>
2023-04-13 05:06:21 +0200

    task 4>>corr

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,9 +4,11 @@ This module is a simple Flask application that defines several routes:
 Routes:
 - The homepage ("/") displays the message "Hello HBNB!" when visited.
 - The "/hbnb" page displays the message "HBNB" when visited.
-- The "/c/<text>" page is a dynamic route that replaces underscores with spaces in the <text> parameter.
+- The "/c/<text>" page is a dynamic route that replaces underscores
+with spaces in the <text> parameter.
 - The "/python/<text>" page is a dynamic route that displays "Python"
-followed by a custom text string. If no text string is provided, the default message "is cool" is used.
+followed by a custom text string. If no text string is provided,
+the default message "is cool" is used.
 - The "/number/<int:n>" page is a dynamic route that
 displays the message "<n> is a number" when visited, where <n> is a provided integer.
 
@@ -21,7 +23,7 @@ app = Flask(__name__)
 def Hello_HBNB():
     """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
-    
+  
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
@@ -40,11 +42,13 @@ def C(text):
 def python(text):
     """Displays 'Python'"""
     return 'Python {}'.format(text.replace('_', ' '))
-       
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """Displays a number"""
     return "{} is a number".format(n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)

1f3dc129ab37ea4c5c8d4f6d882c5e340ef6e4e7
nadaMAoui <maaouinada747@gmail.com>
2023-04-14 21:49:15 +0200

    <<>>

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-"""Simple application"""
+"""
+Flask web application that retrieves all states from a database and renders them in an HTML template
+"""
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown_data(self):
-    """Refresh data"""
+    """
+    Closes the database session after each request
+    """
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Fetch all states"""
+    """
+    Fetch all states from the database and
+    render them in an HTML template
+    """
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")

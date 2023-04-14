062b195d6aee238305100b66cd84ebef2cfada7f
nadaMAoui <maaouinada747@gmail.com>
2023-04-14 21:25:26 +0200

    >>

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""Simple application"""
+"""Flask web application that retrieves data from a database and displays it"""
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
-storage.all()
 
 
 @app.teardown_appcontext
 def teardown_data(self):
-    """Refresh data"""
+    """Closes the database session after each request"""
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """ return all cities in the db  """
+    """
+    Retrieve all the states and the cities
+    in each state from the database and
+    render them in a template
+    """
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
 

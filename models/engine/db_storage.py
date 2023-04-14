b8b2e59a52ab57094e70fd135199cd9e8d58ed8d
nadaMAoui <maaouinada747@gmail.com>
2023-04-14 14:43:44 +0200

    >>

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -91,11 +91,14 @@ class DBStorage:
         """
         create all tables in the database and the current database session
         """
+        if self.__session:
+            self.__session.close()
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
 
     def close(self):
         """close session"""

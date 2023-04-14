7e8b8fd1ea4ebe8ebf1b9cd7fe68c31ed766626c
nadaMAoui <maaouinada747@gmail.com>
2023-04-14 21:16:31 +0200

    correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
-''' Flask web application that displays
-a list of states from a database '''
+''' This module contains a Flask web application that displays a list of states from a database. '''
 
+#!/usr/bin/python3
 from flask import Flask, render_template
 from models.state import State
 from models import storage
@@ -10,13 +9,17 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def state_list():
-    ''' Flask view function'''
+    ''' Flask view function that retrieves
+    all states from the database
+    and passes them to the HTML template
+    to generate an HTML response '''
     all_states = storage.all(State)
     return render_template('7-states_list.html', states=all_states)
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
-    ''' Function '''
+    ''' Function to close the database connection
+    when the app context is torn down '''
     return storage.close()
 
 if __name__ == '__main__':

8f1d257074f02ac712d780cfa44c980dac3fecda
nadaMAoui <maaouinada747@gmail.com>
2023-04-14 21:05:10 +0200

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-''' Flask web application '''
+'''Flask web application that displays
+a list of states from a database'''
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from models.state import State
-from models.state import City
 from models import storage
+
 
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_state():
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    '''Flask view function that retrieves
+    all states from the database
+    and passes them to the HTML template
+    to generate an HTML response'''
     all_states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=all_states)
+    return render_template('7-states_list.html', states=all_states)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
+    '''Function to close the database connection
+    when the app context is torn down'''
     return storage.close()
 
 

37c603d3fe0c7f20ede215eb852a5778937b79c1
nadaMAoui <maaouinada747@gmail.com>
2023-07-06 17:48:52 +0200

    correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,7 +7,7 @@ from models import storage
 """
 You must use storage for fetching data
 from the storage engine (FileStorage or DBStorage) =>
- from models import storage and storage.all(...)
+from models import storage and storage.all(...)
 """
 
 
@@ -16,14 +16,21 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-        storage.close()
+    """
+    Closes the storage connection after each request.
+    """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """etching data from the storage engine"""
+    """
+    Fetches data from the storage engine and renders the template.
+    """
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 

b09beb2e2af455a893fcb5cefa907cc3c91741e6
nadaMAoui <maaouinada747@gmail.com>
2023-07-06 17:35:09 +0200

    updating

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
 """
-Flask web application that retrieves a list of states from a database
-and displays them in an HTML template
+You must use storage for fetching data
+from the storage engine (FileStorage or DBStorage) =>
+ from models import storage and storage.all(...)
 """
 from flask import Flask, render_template
-from os import environ
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
-environ['FLASK_ENV'] = 'development'
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """
-    Closes the database session after each request
-    """
-    storage.close()
+def teardown_db(exception):
+        storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-    Retrieves all states from the database
-    and passes them to the HTML template to generate an HTML response
-    """
-    all_states = storage.all(State)
-    return render_template('7-states_list.html', states=all_states)
-
-
+    """etching data from the storage engine"""
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=states)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
